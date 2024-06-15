54f8f44307f7ecc5a5513aa64658f13dff50fbff
f-rousset <francois.rousset@umontpellier.fr>
2024-06-15 19:12:27 +0200

    v1.0.2 renamed variables

--- package/R/utilities.R
+++ package/R/utilities.R
@@ -1,36 +1,39 @@
 
-get_py_MAF_envir <- function(py_envir, reset=FALSE, torch_device="cpu") {
-  if (reset || ! py_envir$is_set) {
+get_py_MAF_handle <- function(envir, reset=FALSE, torch_device="cpu") {
+  if (reset || ! envir$is_set) {
     cat("\nInitializing python session... ")
     MAF_density_estimation <- MAF_conditional_density_estimation <- NULL
     # reticulate::source_python(paste0(Infusion::projpath(),"/../MAF-R/MAF.py"))
     infile <- system.file('python', "MAF.py", package='mafR')
     reticulate::source_python(infile)
-    py_envir$MAF_density_estimation <- MAF_density_estimation
-    py_envir$MAF_conditional_density_estimation <- MAF_conditional_density_estimation
-    py_envir$is_set <- TRUE
+    envir$MAF_density_estimation <- MAF_density_estimation
+    envir$MAF_conditional_density_estimation <- MAF_conditional_density_estimation
+    envir$is_set <- TRUE
     ## Python packages to be called from R
-    py_envir$torch <- reticulate::import("torch")
+    envir$torch <- reticulate::import("torch")
+    envir$gc <- reticulate::import("gc")
     #
-    py_envir$device <- py_envir$torch$device(torch_device) 
+    envir$device <- envir$torch$device(torch_device) 
+    # Handle to the eval environ of main Python module:
+    envir$py_main <- reticulate::import_main(convert = FALSE) 
     cat("done.\n")
   }
-  py_envir
+  envir
 }
 
-r_to_torch <- function(x, py_MAF_env, device) {
+r_to_torch <- function(x, py_handle, device) {
   x <- reticulate::r_to_py(x) # to numpy.ndarray...
   x <- x$copy() # "he given NumPy array is not writable, and PyTorch does not support non-writable tensors."
-  x <- py_MAF_env$torch$from_numpy(x) # to torchtensor...
+  x <- py_handle$torch$from_numpy(x) # to torch tensor...
   if (device != "cpu") x <- x$to(device)
   x <- x$float()
   return(x)
 }
 
 control_py_env <- function(seed=NULL) {
-  py_env <- get_py_MAF_envir()
+  py_handle <- get_py_MAF_handle()
   if( ! is.null(seed)) {
-    abyss <- py_env$torch$random$manual_seed(as.integer(seed))
+    abyss <- py_handle$torch$random$manual_seed(as.integer(seed))
   }
   invisible(NULL)
 }
