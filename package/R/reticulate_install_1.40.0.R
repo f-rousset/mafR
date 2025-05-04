246f0a4afc33f0cbf4ae440c3ffbe995c3c2a40f
f-rousset <francois.rousset@umontpellier.fr>
2025-05-04 22:29:40 +0200

    v1.1.8 update doc and scripts wrt reticulate changes

---
+++ package/R/reticulate_install_1.40.0.R
@@ -0,0 +1,47 @@
+# For comments on wsl see infos in my langages/wsl/ subdir
+.reticulate_install_1.40.0 <- function(cuda=FALSE, pip=TRUE, test_cuda=cuda,
+                              test_dynamo=TRUE) {
+  reticulate::install_miniconda() # should be able to control the path?
+  reticulate::py_install(packages="scikit-learn", pip=pip) # to import *sklearn*
+  reticulate::py_install(packages="matplotlib", pip=pip) # 
+  reticulate::py_install(packages="plotnine", pip=pip) # 
+  reticulate::py_install(packages="torch", pip=pip) #
+  reticulate::py_install(packages="zuko", pip=pip) #
+  if (cuda) {
+    # https://rdrr.io/cran/aifeducation/src/R/install_and_config.R
+    reticulate::conda_install(
+      packages = c(
+        "pytorch",
+        paste0("pytorch-cuda","=","12.1")),
+      channel=c("pytorch","nvidia"),
+      conda = "auto",
+      pip = FALSE) # this may be slow, be patient.
+  }
+  if (test_cuda) {
+    Itorch <- reticulate::import("torch")
+    Itorch$tensor(1, device = "cuda")
+  }
+  if (test_dynamo) {
+    reticulate::import("torch._dynamo")
+    # If ImportError: cannot import name 'NP_SUPPORTED_MODULES' from 'torch._dynamo.utils'
+    # then try something like 
+    # sudo ln -sf /home/francois/.local/share/r-miniconda/lib/libstdc++.so.6.0.29 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
+  }
+}
+
+# If reticulate::import("pytorch._dynamo")
+
+## To select version in conda_install, use '==':
+# reticulate::conda_install(
+#   packages = c(
+#     "zuko",
+#     paste0("zuko","==","1.2.0")),
+#   conda = "auto",
+#   pip = TRUE)
+
+# On cluster with limited disk space in home directory: 
+# genotoul installs r-reticulate in /home/frousset/.local/
+# so we create a symbolic link as suggested in 
+#    https://bioinfo.genotoul.fr/index.php/faq/software_faq/ (cf 'overquota')
+# mkdir ~/work/.local
+# ln -s ~/work/.local   ~/.local
--- package/R/reticulate_install_1.42.0.R
+++ package/R/reticulate_install_1.42.0.R
@@ -1,22 +1,22 @@
 # For comments on wsl see infos in my langages/wsl/ subdir
-.install_py_stuff <- function(cuda=FALSE, pip=TRUE, test_cuda=cuda,
+.reticulate_install_1.42.0 <- function(cuda=FALSE, pip=TRUE, test_cuda=cuda,
                               test_dynamo=TRUE) {
   reticulate::install_miniconda() # should be able to control the path?
-  reticulate::py_install(packages="scikit-learn", pip=pip) # to import *sklearn*
-  reticulate::py_install(packages="matplotlib", pip=pip) # 
-  reticulate::py_install(packages="plotnine", pip=pip) # 
-  reticulate::py_install(packages="torch", pip=pip) #
-  reticulate::py_install(packages="zuko", pip=pip) #
-  if (cuda) {
-    # https://rdrr.io/cran/aifeducation/src/R/install_and_config.R
-    reticulate::conda_install(
-      packages = c(
-        "pytorch",
-        paste0("pytorch-cuda","=","12.1")),
-      channel=c("pytorch","nvidia"),
-      conda = "auto",
-      pip = FALSE)
-  }
+  # Sys.getenv("UV_INDEX")
+  ## See recipe for case 'Windows_torch' in keras3::use_backend() ;
+  ## here assuming the GPU requires cuda v12.1 (.../cu126 not tried)
+  Sys.setenv(
+    "UV_INDEX" = 
+      trimws(paste(sep = " ",
+                   "https://download.pytorch.org/whl/cu121",
+                   Sys.getenv("UV_INDEX")
+      )))
+  reticulate::py_require(packages="scikit-learn") # to import *sklearn*
+  reticulate::py_require(packages="matplotlib") # 
+  reticulate::py_require(packages="plotnine") # 
+  reticulate::py_require(c("tensorflow", "torch", "torchvision", "torchaudio"))
+  reticulate::py_require(packages="zuko") #
+
   if (test_cuda) {
     Itorch <- reticulate::import("torch")
     Itorch$tensor(1, device = "cuda")
@@ -28,16 +28,6 @@
     # sudo ln -sf /home/francois/.local/share/r-miniconda/lib/libstdc++.so.6.0.29 /usr/lib/x86_64-linux-gnu/libstdc++.so.6
   }
 }
-
-# If reticulate::import("pytorch._dynamo")
-
-## To select version in conda_install, use '==':
-# reticulate::conda_install(
-#   packages = c(
-#     "zuko",
-#     paste0("zuko","==","1.2.0")),
-#   conda = "auto",
-#   pip = TRUE)
 
 # On cluster with limited disk space in home directory: 
 # genotoul installs r-reticulate in /home/frousset/.local/
